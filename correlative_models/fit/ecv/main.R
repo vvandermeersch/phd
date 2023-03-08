71f49bf1f57fa48173014a980922ae3e59af5bc3
Victor Van der Meersch <victor.vandermeersch@cefe.cnrs.fr>
2023-03-08 10:21:56 +0100

    main file to run calibration with 5-f BC val

---
+++ correlative_models/fit/ecv/main.R
@@ -0,0 +1,51 @@
+#-------------------------------#
+# Main script to run all models #
+#-------------------------------#
+
+# Run correlative models with blockCV environmental clustering 
+# Evaluate the ability of the model for situations of extrapolation (in predictor space)
+
+# Author: V. Van der Meersch
+# Date: 02/03/2023
+
+
+
+wd <- "C:/Users/vandermeersch/Documents/CEFE/phd/correlative_models/fit/ecv"
+set.seed(1997)
+
+
+
+### 0. Parameters --------------- #
+
+sp_name <- "fagus_sylvatica" # species name
+nb_background <- 50000 # number of background points
+
+cov_type <- "finalcov" # name of covariate set
+bc_covars <- c("bio6", "bio12") # bioclim predictors
+soil_covars <- c("WHC", "pH") # soil predictors
+cc_covars <- c("sum_apsep_GDD5", "w_bal") # custom climatic predictors
+covars <- c(bc_covars, soil_covars, cc_covars)
+
+
+
+### 1. Initial setup --------------- #
+
+source(file.path(wd, "init.R"))
+
+
+
+### 2. Run models --------------- #
+
+## Lasso GLM
+source(file.path(wd, "lasso_glm", "lasso_glm.R"))
+
+## GAM
+source(file.path(wd, "gam", "gam.R"))
+
+## Boosted regression tree
+source(file.path(wd, "brt", "brt.R"))
+
+## Random forest
+source(file.path(wd, "random_forest", "random_forest.R"))
+
+
