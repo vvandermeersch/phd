cb358b9914e2405cec56b8779d3a1eaf322ae090
Victor Van der Meersch <victor.vandermeersch@cefe.cnrs.fr>
2023-03-08 10:22:46 +0100

    BRT with 5fold EBC

---
+++ correlative_models/fit/ecv/brt/brt.R
@@ -0,0 +1,177 @@
+
+#----------------------------------------------------#
+# Script to perform a BRT as in Valavi et al. (2022) #
+#----------------------------------------------------#
+
+# this script is largely inspired by Valavi et al. R code 
+
+
+# 1. Common parameters
+ntrees <- 50
+tcomplexity <- 5
+lrate <- 0.001
+
+# all species points
+sp_data <- presabs_data_norm # (use normalized covariates)
+
+# all Europe
+all_data <- europe_data_norm # (use normalized covariates)
+
+# dataframe to combine test predictions
+# (as in Valavi et al. (2023), we accumulate predictions to test data)
+accumulate_data <- data.frame(pres = model_data$pres, pred = NA)
+
+
+# 2. Run calibrations
+cat("Boosted Regression Tree calibrations\n\n")
+cat("   Fivefold environmental cross-validation\n")
+for(k in seq_len(length(folds))){
+  cat(paste0("    - Fold ", k, "\n"))
+  train_set <- unlist(folds[[k]][1]) # training set indices
+  test_set <- unlist(folds[[k]][2]) # testing set indices
+  
+  train_data <- model_data_norm[train_set, ] # (use normalized covariates)
+  test_data <- model_data_norm[test_set, ] 
+  
+  # calculating the case weights (down-weighted)
+  prNum <- as.numeric(table(train_data$pres)["1"]) # number of presences
+  bgNum <- as.numeric(table(train_data$pres)["0"]) # number of backgrounds
+  wt <- ifelse(train_data$pres == 1, 1, prNum / bgNum)
+  
+  # run model
+  t0 <- proc.time()
+  mod_brt <- gbm.step(data = train_data, 
+                      gbm.x = 1:6,
+                      gbm.y = 7, 
+                      family = "bernoulli",
+                      site.weights = wt,
+                      tree.complexity = tcomplexity,
+                      learning.rate = lrate,
+                      n.trees = ntrees,
+                      n.folds = 5,
+                      max.trees = 10000,
+                      silent = TRUE)
+  runtime <- proc.time() - t0
+  
+  # predict test set
+  test_data$pred <- predict(mod_brt, test_data, n.trees = mod_brt$gbm.call$best.trees, type = "response")
+  eval_obj <- evalmod(scores = test_data$pred, labels = test_data$pres)
+  auc_test <- precrec::auc(eval_obj)
+  cat(paste0("      Testing AUC = ", round(auc_test$aucs[1],2), "\n"))
+  
+  # adding test predictions
+  accumulate_data[test_set, "pred"] <- test_data$pred
+  
+  # predict on all species points
+  sp_data$pred <- predict(mod_brt, sp_data, n.trees = mod_brt$gbm.call$best.trees, type = "response")
+  eval_obj <- evalmod(scores = sp_data$pred, labels = sp_data$pres)
+  auc_all <- precrec::auc(eval_obj)
+  cat(paste0("      Total AUC = ", round(auc_all$aucs[1],2), "\n"))
+  
+  # best threshold
+  youden_index <- sensitivity(sp_data$pred, as.factor(sp_data$pres), perc.rank = F)$measure +
+    specificity(sp_data$pred, as.factor(sp_data$pres), perc.rank = F)$measure - 1
+  thresholds <- sensitivity(sp_data$pred, as.factor(sp_data$pres), perc.rank = F)$cutoffs
+  best_threshold <- thresholds[which(youden_index == max(youden_index))]
+  
+  # predict on all Europe
+  all_data$pred <- predict(mod_brt, all_data, n.trees = mod_brt$gbm.call$best.trees, type = "response")
+  
+  # save file
+  outfile <- list()
+  outfile$species <- sp_name # species name
+  outfile$name <- "Boosted Regression Tree" # model name
+  outfile$modality <- "ecv" # modelling modality 
+  outfile$fit_date <- Sys.Date() # date
+  outfile$model <- mod_brt # model object
+  outfile$runtime <- runtime[3] # runtime
+  outfile$auc_test <- auc_test # auc on testing set
+  outfile$auc_all <- auc_all # auc on every species points
+  outfile$best_threshold <- best_threshold # best threshold to discriminate probabilites
+  outfile$europe_pred <- data.frame(lat = alt$lat, lon = alt$lon, pred = all_data$pred) # prediction on every Europe cells
+  outfile$cov_norm <- TRUE # are covariates normalized before calibration ?
+  outfile$meanv_l <- meanv_l # mean parameter list for normalization
+  outfile$sdv_l <- sdv_l # standard deviation parameter list for normalization
+  
+  # save file
+  dir.create(paste0(wd, "/brt/fit/", sp_name), showWarnings = FALSE)
+  saveRDS(outfile, file = paste0(wd, "/brt/fit/", sp_name, "/brt_", cov_type, "_fold", k,".rds"))
+  
+  gc()
+  
+}
+
+# combined set of test observations and predictions to evaluate the performance once
+eval_obj <- evalmod(scores = accumulate_data$pred, labels = accumulate_data$pres)
+auc_accumulate <- precrec::auc(eval_obj)
+boyce_accumulate <- ecospat.boyce(fit = accumulate_data$pred, obs = accumulate_data[accumulate_data$pres == 1,]$pred, 
+                                  nclass=0, window.w="default", res=100, 
+                                  PEplot = F, rm.duplicate = F,  method = 'pearson' )
+
+
+# 3. Full model (all the available training data is used)
+# "this approach favours final prediction quality over perfect accuracy of error estimates", see Roberts et al. (2017)
+
+full_data <- model_data_norm # (use normalized covariates)
+cat("\n   Full model\n")
+cat(paste0("      AUC on  combined set of test predictions = ", round(auc_accumulate$aucs[1],2),"\n"))
+cat(paste0("      Boyce index on combined set of test predictions = ", round(boyce_accumulate$cor,2),"\n"))
+
+# convert the response to factor for RF model to return probabilities
+full_data$pres <- as.factor(full_data$pres)
+
+# calculating the case weights (down-weighted)
+prNum <- as.numeric(table(train_data$pres)["1"]) # number of presences
+bgNum <- as.numeric(table(train_data$pres)["0"]) # number of backgrounds
+wt <- ifelse(train_data$pres == 1, 1, prNum / bgNum)
+
+# run model
+t0 <- proc.time()
+mod_brt <- gbm.step(data = full_data, 
+                    gbm.x = 1:6,
+                    gbm.y = 7, 
+                    family = "bernoulli",
+                    site.weights = wt,
+                    tree.complexity = tcomplexity,
+                    learning.rate = lrate,
+                    n.trees = ntrees,
+                    n.folds = 5,
+                    max.trees = 10000,
+                    silent = TRUE)
+runtime <- proc.time() - t0
+
+# predict on all species points
+sp_data$pred <- predict(mod_brt, sp_data, n.trees = mod_brt$gbm.call$best.trees, type = "response")
+eval_obj <- evalmod(scores = sp_data$pred, labels = sp_data$pres)
+auc_all <- precrec::auc(eval_obj)
+cat(paste0("      Total AUC = ", round(auc_all$aucs[1],2), "\n"))
+
+# best threshold
+youden_index <- sensitivity(sp_data$pred, as.factor(sp_data$pres), perc.rank = F)$measure +
+  specificity(sp_data$pred, as.factor(sp_data$pres), perc.rank = F)$measure - 1
+thresholds <- sensitivity(sp_data$pred, as.factor(sp_data$pres), perc.rank = F)$cutoffs
+best_threshold <- thresholds[which(youden_index == max(youden_index))]
+
+# predict on all Europe
+all_data$pred <- predict(mod_brt, all_data, n.trees = mod_brt$gbm.call$best.trees, type = "response")
+
+# save file
+outfile <- list()
+outfile$species <- sp_name # species name
+outfile$name <- "Boosted Regression Tree" # model name
+outfile$modality <- "ecv - full model" # modelling modality 
+outfile$fit_date <- Sys.Date() # date
+outfile$model <- mod_brt # model object
+outfile$runtime <- runtime[3] # runtime
+outfile$auc_test <- auc_accumulate # auc on  combined set of test predictions
+outfile$boyceindex_test <- boyce_accumulate # Boyce Index on  combined set of test predictions
+outfile$auc_all <- auc_all # auc on every species points
+outfile$best_threshold <- best_threshold # best threshold to discriminate probabilites
+outfile$europe_pred <- data.frame(lat = alt$lat, lon = alt$lon, pred = all_data$pred) # prediction on every Europe cells
+outfile$cov_norm <- TRUE # are covariates normalized before calibration ?
+outfile$meanv_l <- meanv_l # mean parameter list for normalization
+outfile$sdv_l <- sdv_l # standard deviation parameter list for normalization
+
+saveRDS(outfile, file = paste0(wd, "brt/fit/", sp_name, "/brt_", cov_type, "_fullmodel.rds"))
+
+gc()
