ad6e231a729ccfe0adafeb71ee2e85008823a741
Victor Van der Meersch <victor.vandermeersch@cefe.cnrs.fr>
2023-03-30 15:56:31 +0200

    new simulations with phenofit

--- hindcasting/simulation/phenofit/plot_simulations.R
+++ hindcasting/simulation/phenofit/plot_simulations.R
@@ -21,7 +21,7 @@ mod <- readRDS("C:/Users/vandermeersch/Documents/CEFE/phd/phenofit/fit/forward/f
 
 # loop on years
 
-for(year in c(500, 1000, 1500, 2000, 3000, 3500, 4000, 4500, 5000, 5500, 6000)){
+for(year in c(6500, 7000, 7500, 8000, 8500, 9000)){
   
   fitness <- readRDS(file.path(output_folder, paste0(year, "BP.rds")))
   pollen <- readRDS(file.path(pollen_folder, paste0("pres_", year, "BP.rds")))
@@ -55,36 +55,36 @@ for(year in c(500, 1000, 1500, 2000, 3000, 3500, 4000, 4500, 5000, 5500, 6000)){
   ggsave(plot = plot, 
          filename = file.path(wd, "phenofit", "output", paste0("fitnessplot_", year, "BP.png")), 
          device = "png", width = 7.5, height = 7, bg = "white")
+  
+  # best threshold
+  ths <- mod$best_threshold
+  
+  fitness$bin_pred <- 0
+  fitness[fitness$pred >= ths, "bin_pred"] <- 1
+  plot <- ggplot() +
+    geom_raster(data = fitness, aes(fill = bin_pred, x = lon, y = lat)) +
+    geom_point(data = pollen[pollen$pres == 1,], aes(x = lon, y = lat), col = "white", fill = "#009B72", shape = 21, size = 1) +
+    geom_point(data = pollen[pollen$pres == 0,], aes(x = lon, y = lat), col = "white", shape = 4, size = 0.5) +
+    theme_void() +
+    ylab("") +
+    xlab("") +
+    scale_fill_gradientn(colours = c("#ff9ea0", "#f8961e", "#f9c74f", "#43aa8b", "#577590"), breaks = c(0,0.25,0.5,0.75,1), limits = c(0,1)) +
+    guides(fill = guide_colorbar(title.position = "top", direction = "horizontal", frame.colour = "black", frame.linewidth = 0.3,
+                                 ticks = FALSE)) +
+    theme(legend.title=element_blank(), legend.position = "none", 
+          legend.key.height = unit(0.2, 'cm'), legend.key.width = unit(0.5, 'cm')) +
+    new_scale_fill() + 
+    geom_raster(data = ice_sheet, aes(fill = ice, x = lon, y = lat), alpha = 1) +
+    scale_fill_gradient2(low = "#e9e8ef", mid = "#c9ccde", high = "#a6abd0", limits = c(0,1.1), breaks = c(0,0.5,1), midpoint = 0.5) +
+    geom_rect(aes(xmin = -13.3, xmax = -6.8, ymin = 71, ymax = 72.9), 
+              fill = "grey", alpha = 0.4, color = "black") +
+    annotate("text", x = -10, y = 72, label = paste0(year, " BP")) 
+  
+  
+  ggsave(plot = plot, 
+         filename = file.path(wd, "phenofit", "output", paste0("presplot_", year, "BP.png")), 
+         device = "png", width = 7.5, height = 7, bg = "white")
 }
 
-
-scale_fill_gradientn(colours = c("#f94144", "#f8961e", "#f9c74f", "#43aa8b", "#577590"), breaks = c(0,0.25,0.5,0.75,1), limits = c(0,1))
-
-
-  scale_fill_gradient2(low = "#edede1", mid = "#B4E0AA", high = "#488B49", limits = c(0,1), breaks = c(0,0.5,1), midpoint = 0.5) +
-
-ggplot() +
-  geom_raster(data = altitude, aes(fill = alt, x = lon, y = lat)) +
-  scale_fill_gradient(low = alpha("black", 0.5), high = "white") +
-  new_scale_fill() + 
-  geom_raster(data = fitness, aes(fill = value, x = lon, y = lat), alpha = 0.8) +
-  theme_void() +
-  scale_fill_gradient2(low = "#EBEBD3", mid = "#B4E0AA", high = "#488B49", limits = c(0,1), breaks = c(0,0.5,1), midpoint = 0.5)
-
-
-+
-  ylab("") +
-  xlab("") +
-  scale_fill_gradient2(low = "#EBEBD3", mid = "#B4E0AA", high = "#488B49", limits = c(0,1), breaks = c(0,0.5,1), midpoint = 0.5) +
-  guides(fill = guide_colorbar(title.position = "top", direction = "horizontal", frame.colour = "black", frame.linewidth = 0.3,
-                               ticks = FALSE)) +
-  theme(legend.title=element_blank(), legend.position = "none", 
-        legend.key.height = unit(0.2, 'cm'), legend.key.width = unit(0.5, 'cm')) +
-  new_scale_fill() + 
-  geom_raster(data = ice_sheet, aes(fill = ice, x = lon, y = lat), alpha = 0.7) +
-  scale_fill_gradient2(low = "#e9e8ef", mid = "#c9ccde", high = "#a6abd0", limits = c(0,1.1), breaks = c(0,0.5,1), midpoint = 0.5) +
-  geom_rect(aes(xmin = -13.3, xmax = -6.8, ymin = 71, ymax = 72.9), 
-            fill = "grey", alpha = 0.4, color = "black") +
-  annotate("text", x = -10, y = 72, label = paste0(year, " BP")) 
 
 
--- hindcasting/simulation/phenofit/run_simulations.R
+++ hindcasting/simulation/phenofit/run_simulations.R
@@ -16,8 +16,8 @@ source(file.path(wd, "functions/read_mean_outputvalue.R"))
 species_file <- "C:/Users/vandermeersch/Dropbox/These_Victor/Phenofit4/species/forward/Fagus_sylvatica_VVanderMeersch.species"
 # species_file <- "D:/calibrations/phenofit/fagus_sylvatica/1000pres_1000abs/paper_data/CMAES/subset_4/cmaes_fit_subset4_rep1.species"
 # species_file <- "C:/Users/vandermeersch/Dropbox/These_Victor/Phenofit4/species/forward/Abies_alba_VVanderMeersch2.species"
-output_folder <- "D:/simulations/phenofit/paleo/05deg"
-climate_folder <- "D:/climate/HadCM3B_60Kyr_Climate/2023_dataset/phenofit_format/05deg"
+output_folder <- "D:/simulations/phenofit/paleo/05deg_wocor"
+climate_folder <- "D:/climate/HadCM3B_60Kyr_Climate/2023_dataset/phenofit_format/05deg_wocor"
 command_file <- file.path(wd, "phenofit","command_file.txt")
 
 
@@ -30,7 +30,7 @@ run <- paste(capsis_settings$cd_java8, capsis_settings$java8, capsis_settings$cd
 
 # simulation loop
 
-for(year in c(2500)){
+for(year in c(5000)){
   yr_b <- as.character(- year - 14)
   yr_e <- as.character(- year + 15)
   command_file_setup(command_file, species_file, output_folder, climate_folder,
