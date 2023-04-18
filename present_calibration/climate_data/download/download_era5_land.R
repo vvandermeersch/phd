861eb38a8958dad8f5a1acbe10afeee23a633b5b
Victor Van der Meersch <victor.vandermeersch@cefe.cnrs.fr>
2023-04-18 16:08:12 +0200

    cleaning

--- present_calibration/climate_data/download/download_era5_land.R
+++ present_calibration/climate_data/download/download_era5_land.R
@@ -1,6 +1,4 @@
 library(reticulate)
-library(future)
-library(future.apply)
 
 setwd("D:/climate/ERA5-Land/raw") # folder where files will be downloaded
 
@@ -16,21 +14,18 @@ var <- '10m_u_component_of_wind'
 # For valid keywords, see Table 2 of:
 # https://confluence.ecmwf.int/display/CKB/ERA5-Land%3A+data+documentation#ERA5Land:datadocumentation-parameterlistingParameterlistings
 
-
-
-
 # Select years
-years <- as.character(2001:2020)
+years <- as.character(1970:2000)
 
 # Maximum number of per-user requests that access the online CDS data is 2
 # plan(multisession, workers = 2) ?!
 # But Python from reticulate cannot run in parallel because of external pointers !
-# We have to launch manually 2 R sessions
-years_1 <- years[1:(length(years)%/%2)]
-years_2 <- years[(length(years)%/%2+1):length(years)]
+# We can launch manually 2 R sessions, with these two subsets:
+# years_1 <- years[1:(length(years)%/%2)]
+# years_2 <- years[(length(years)%/%2+1):length(years)]
 
 #Run script
-era5_land(years = years_1, var = var)
+era5_land(years = years, var = var)
 
 
 
