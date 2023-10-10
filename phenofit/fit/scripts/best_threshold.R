d72dd76c3065a43e37b450ddca1c65d058993a14
Victor Van der Meersch <victor.vandermeersch@cefe.cnrs.fr>
2023-10-10 11:59:20 +0200

    PHENOFIT fit parameters

--- phenofit/fit/scripts/best_threshold.R
+++ phenofit/fit/scripts/best_threshold.R
@@ -7,23 +7,19 @@ library(dplyr)
 source(file.path("C:/Users/vandermeersch/Documents/CEFE/phd/hindcasting/simulation", "functions/read_mean_outputvalue.R"))
 
 sp_folder <- "D:/species/processed"
-sp_presabs <- readRDS(file.path(sp_folder, "quercus_pubescens/quercus_pubescens_presabs.rds"))
-sp_name <- "quercus_pubescens"
+sp_presabs <- readRDS(file.path(sp_folder, "quercus_petraea/quercus_petraea_presabs.rds"))
+sp_name <- "quercus_petraea"
 
-
-
-out_folder <- "C:/Users/vandermeersch/Documents/CEFE/phd/phenofit/fit/expert/quercus_pubescens"
-#sim_folder <- "D:/simulations/phenofit/present/fitted/quercus_pubescens/subset1_rep2"
-# sim_folder <- "D:/simulations/phenofit/present/fitted/quercus_ilex/paper_data/CMAES/subset1_rep1"
-sim_folder <- "D:/simulations/phenofit/present/expert/quercus_pubescens/BLeys"
-# filename <- "cmaes_fit_subset2_rep1"
-filename <- "Quercus_pubescens_BLeys"
-modality <- "expert calibration"
+out_folder <- "C:/Users/vandermeersch/Documents/CEFE/phd/phenofit/fit/fitted/quercus_petraea"
+sim_folder <- "D:/simulations/phenofit/present/fitted/quercus_petraea/subset1_rep2"
+filename <- "cmaes_fit_subset1_rep2"
+# filename <- "Quercus_ilex_FTauc_ptype3"
+modality <- "inverse calibration"
 
 # Compute AUC on every pres/abs points
 fitness <- read_mean_outputvalue(sim_folder, output_var = "Fitness")
 names(fitness)[3] <- "pred"
-fitness_presabs <- inner_join(sp_presabs, fitness, by = c("lat", "lon")) %>% dplyr::select(-c('pres'))
+fitness_presabs <- inner_join(sp_presabs, fitness, by = c("lat", "lon"))
 auc_tot <- round(auc(roc(fitness_presabs$pred, as.factor(sp_presabs$pres))),6)
 print(auc_tot)
 
@@ -34,6 +30,17 @@ thresholds <- sensitivity(fitness_presabs$pred, as.factor(sp_presabs$pres), perc
 best_threshold <- thresholds[which(youden_index == max(youden_index))]
 print(best_threshold)
 
+# Confusion matrix and TSS
+fitness_presabs$pred_pres <- ifelse(fitness_presabs$pred < best_threshold,0 , 1)
+tp <- nrow(fitness_presabs[fitness_presabs$pred_pres == 1 & fitness_presabs$pres == 1,])
+fp <- nrow(fitness_presabs[fitness_presabs$pred_pres == 1 & fitness_presabs$pres == 0,])
+tn <- nrow(fitness_presabs[fitness_presabs$pred_pres == 0 & fitness_presabs$pres == 0,])
+fn <- nrow(fitness_presabs[fitness_presabs$pred_pres == 0 & fitness_presabs$pres == 1,])
+mig_sens = tp/(tp+fn)
+mig_spec = tn/(tn+fp)
+tss = mig_sens + mig_spec - 1
+print(tss)
+
 # save (as in CSDM)
 outfile <- list()
 outfile$species <- sp_name
@@ -41,6 +48,7 @@ outfile$name <- "PHENOFIT" # model name
 outfile$modality <- modality # modelling modality 
 outfile$species_file <- filename
 outfile$auc_all <- auc_tot # auc on every species points
+outfile$tss_all <- tss # tss on every species points
 outfile$best_threshold <- best_threshold # best threshold to discriminate probabilites
 outfile$europe_pred <- fitness # prediction on every Europe cells
 saveRDS(outfile, file = file.path(out_folder, paste0(filename, ".rds")))
